b100ae235c5ac8e7ebace450ca17dae5125c027b
Vana223 <104148733+Vana223@users.noreply.github.com>
2024-01-24 16:42:07 +0000

    Commit

---
+++ src/Quotation.java
@@ -0,0 +1,8 @@
+public class Quotation {
+    String quote = "The only thing worse than being blind is having sight but no vision.";
+    String author = "– Helen Keller";
+    public void display() {
+      System.out.println(quote);
+      System.out.println(author);
+    }
+  }
